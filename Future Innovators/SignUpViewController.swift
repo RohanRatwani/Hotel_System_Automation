e365f6d8a54f426c03f07fb6d08656e8acb9e0f5
Jay Mehta <jaymmehta97@gmail.com>
2019-03-02 16:07:24 +0530

    Updates:- DOB Validations Navigation Controller

--- Future Innovators/SignUpViewController.swift
+++ Future Innovators/SignUpViewController.swift
@@ -15,7 +15,9 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var phoneNumber: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
     
+    let datePicker: UIDatePicker = UIDatePicker()
     let tapGesture = UITapGestureRecognizer()
     
     override func viewDidLoad() {
@@ -24,15 +26,33 @@ class SignUpViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: signupButton.backgroundColor]
         tapGesture.addTarget(self, action: #selector(closeKeyboards))
         view.addGestureRecognizer(tapGesture)
-        // Do any additional setup after loading the view.
+        
+        datePicker.timeZone = NSTimeZone.local
+        //datePicker.backgroundColor = UIColor.white
+        datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(_:)), for: .valueChanged)
+        datePicker.datePickerMode = .date
+        dateOfBirthTextField.inputView = datePicker
+        
     }
     
     @objc func closeKeyboards(){
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
+        phoneNumber.resignFirstResponder()
+        dateOfBirthTextField.resignFirstResponder()
+        fullNameTextField.resignFirstResponder()
     }
 
     @IBAction func loginButtonClicked(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        // Apply date format
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        dateOfBirthTextField.text = selectedDate
     }
 }
